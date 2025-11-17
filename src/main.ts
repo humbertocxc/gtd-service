997913facdfb94b6165df1105bd77a967c77a387
humberto <humberto.gnm98@gmail.com>
2025-11-16 21:49:03 -0300

    change default port to 3001

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   );
 
   setupSwagger(app, configService);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 void bootstrap();
